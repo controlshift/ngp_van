be19de72beb4197f20208675c9302a4aea9c6da0
Diego Marcet <anero@users.noreply.github.com>
2019-04-26 19:13:31 -0400

    Add support for removing code from people (#6)

--- lib/ngp_van/client/people.rb
+++ lib/ngp_van/client/people.rb
@@ -30,6 +30,10 @@ module NgpVan
       def apply_code_to_person(id:, body: {})
         post(path: "people/#{esc(id)}/codes", body: body)
       end
+
+      def delete_code_from_person(id:, codeId:)
+        delete(path: "people/#{esc(id)}/codes/#{esc(codeId)}")
+      end
     end
   end
 end
