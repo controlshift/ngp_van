550f7a29266f2202c86a39e3a3118a9511c65089
anero <anero79@gmail.com>
2025-12-22 16:52:37 -0300

    Don't set the errors instance variable unless the response is in JSON format

--- lib/ngp_van/error.rb
+++ lib/ngp_van/error.rb
@@ -45,11 +45,11 @@ module NgpVan
       @response = response
       if response.response_headers['Content-Type']&.match(/application\/json/i)
         @body = ::JSON.parse(response[:body])
+        @errors = body.delete('errors')
       else
         @body = response[:body]
       end
       @status = response[:status]
-      @errors = body.delete('errors')
 
       super(build_error)
     end
